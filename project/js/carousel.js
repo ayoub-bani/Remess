8dfd6460969d61ee79da0159852993312bb3cbb2
ayoub-bani <ayoub.bani101@gmail.com>
2019-07-25 14:47:00 +0100

    membres and partenaires sections done

---
+++ project/js/carousel.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        autoplay: {
+            Type: Boolean,
+            Default: true,
+        },
+        loop: {
+            Type: Boolean,
+            Default: true,
+        },
+    });
+});
